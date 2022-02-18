f5e087372db7ea81d56155cdcf9db9b0a4438168
scyyc7 <scyyc7@nottingham.edu.cn>
2022-02-18 14:47:40 +0800

    CORS fixed

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,14 +4,14 @@ module.exports = {
   ],
   publicPath: './',
   devServer: {
-    port: 8082,
+    port: '8082',
     open: true,
     host: "localhost",
     https: false,
     hotOnly: false,
-    proxyTable: {
+    proxy: {
       '/api': {
-        target: 'http://localhost:8088/api', //API服务器的地址
+        target: 'http://localhost:8088', //API服务器的地址
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
