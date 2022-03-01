7d558eafba504d40f7d119f3f40762f66f74b25e
scyhz7 <scyhz7@nottingham.edu.cn>
2022-03-01 17:15:04 +0800

    Implement the basic comment to generate the javaDoc

--- backend/src/main/java/com/grp202116/backend/statistics/Statistics.java
+++ backend/src/main/java/com/grp202116/backend/statistics/Statistics.java
@@ -12,7 +12,9 @@ import java.math.BigInteger;
 import java.util.List;
 
 /**
- * The class Statistic
+ * The class Statistics contains the basic information of the statistics
+ * controls the statistics of data, labels and project.
+ * The calculation will be done in the backend then send to the frontend to list the Statistics
  */
 public class Statistics {
     @Resource
@@ -31,6 +33,11 @@ public class Statistics {
     List<PredictionDO> predictions;
     List<AnnotationDO> annotations;
 
+    /**
+     * The construct method called once a project is created
+     * and initialize the basic data object
+     * @param projectId the created project id
+     */
     public Statistics(BigInteger projectId){
         this.projectId = projectId;
         this.setDatasNumber(countDatas());
@@ -41,30 +48,58 @@ public class Statistics {
         this.setPredictionsNumber(BigInteger.valueOf(getPredictions().size()));
     }
 
+    /**
+     * Get the number of the data
+     * @return the number of the data
+     */
     public BigInteger getDatasNumber() {
         return datasNumber;
     }
 
+    /**
+     * Set the number of the data
+     * @param datasNumber the number of the data
+     */
     public void setDatasNumber(BigInteger datasNumber) {
         this.datasNumber = datasNumber;
     }
 
+    /**
+     * Get the number of the Annotation
+     * @return the number of the Annotation
+     */
     public BigInteger getAnnotationsNumber() {
         return annotationsNumber;
     }
 
+    /**
+     * Set the number of the Annotation
+     * @param annotationsNumber the number of the Annotation
+     */
     public void setAnnotationsNumber(BigInteger annotationsNumber) {
         this.annotationsNumber = annotationsNumber;
     }
 
+    /**
+     * Get the annotations object list
+     * @return the annotations object list
+     */
     public List<AnnotationDO> getAnnotations() {
         return annotations;
     }
 
+    /**
+     * Set the annotation data object list
+     * @param annotations the annotation data object list
+     */
     public void setAnnotations(List<AnnotationDO> annotations) {
         this.annotations = annotations;
     }
 
+    /**
+     * Get the number of the predictions
+     * @return
+     */
     public BigInteger getPredictionsNumber() {
         return predictionsNumber;
     }
