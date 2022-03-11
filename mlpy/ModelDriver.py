025a97de1143205efe5242ba2b561d5423a572e0
Leon-Leyang <scylh6@nottingham.edu.cn>
2022-03-11 14:58:02 +0800

    Modify the structure of mlpy

--- mlpy/ModelDriver.py
+++ mlpy/ModelDriver.py
@@ -85,8 +85,6 @@ class ModelDriver():
         predictions = []
         result = []
 
-        fromName, toName, toolType = self.parseConfig()
-
         # Map to store class index with its name
         index2nameMap = {
             0: 'Background',
@@ -111,6 +109,8 @@ class ModelDriver():
             19: 'Train',
             20: 'Tv/monitor'
         }
+
+        fromName, toName, toolType = self.parseConfig()
 
         model = torch.load('../ml/models/fcn/fcn.pth')
         model.eval()
@@ -165,11 +165,6 @@ class ModelDriver():
 
 
 
-
-
-
-
-
         # For development checking
         # rle2image = brush.decode_rle(rle)
         # img = rle2image.reshape((734, 1100, 4))
@@ -183,43 +178,32 @@ class ModelDriver():
         # plt.show()
 
 
-
-
-
-
-
-
-
-
-
-
-
     # For development checking
-    def decode_segmap(self,image, nc=21):
-
-        label_colors = np.array([(0, 0, 0),  # 0=background
-                                 # 1=aeroplane, 2=bicycle, 3=bird, 4=boat, 5=bottle
-                                 (128, 0, 0), (0, 128, 0), (128, 128, 0), (0, 0, 128), (128, 0, 128),
-                                 # 6=bus, 7=car, 8=cat, 9=chair, 10=cow
-                                 (0, 128, 128), (128, 128, 128), (64, 0, 0), (192, 0, 0), (64, 128, 0),
-                                 # 11=dining table, 12=dog, 13=horse, 14=motorbike, 15=person
-                                 (192, 128, 0), (64, 0, 128), (192, 0, 128), (64, 128, 128), (192, 128, 128),
-                                 # 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
-                                 (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128)])
-
-        r = np.zeros_like(image).astype(np.uint8)
-        g = np.zeros_like(image).astype(np.uint8)
-        b = np.zeros_like(image).astype(np.uint8)
-
-        for l in range(nc):
-            idx = image == l
-            r[idx] = label_colors[l, 0]
-            g[idx] = label_colors[l, 1]
-            b[idx] = label_colors[l, 2]
-
-
-        rgb = np.stack([r, g, b], axis=2)
-        return rgb
+    # def decode_segmap(self,image, nc=21):
+    #
+    #     label_colors = np.array([(0, 0, 0),  # 0=background
+    #                              # 1=aeroplane, 2=bicycle, 3=bird, 4=boat, 5=bottle
+    #                              (128, 0, 0), (0, 128, 0), (128, 128, 0), (0, 0, 128), (128, 0, 128),
+    #                              # 6=bus, 7=car, 8=cat, 9=chair, 10=cow
+    #                              (0, 128, 128), (128, 128, 128), (64, 0, 0), (192, 0, 0), (64, 128, 0),
+    #                              # 11=dining table, 12=dog, 13=horse, 14=motorbike, 15=person
+    #                              (192, 128, 0), (64, 0, 128), (192, 0, 128), (64, 128, 128), (192, 128, 128),
+    #                              # 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
+    #                              (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128)])
+    #
+    #     r = np.zeros_like(image).astype(np.uint8)
+    #     g = np.zeros_like(image).astype(np.uint8)
+    #     b = np.zeros_like(image).astype(np.uint8)
+    #
+    #     for l in range(nc):
+    #         idx = image == l
+    #         r[idx] = label_colors[l, 0]
+    #         g[idx] = label_colors[l, 1]
+    #         b[idx] = label_colors[l, 2]
+    #
+    #
+    #     rgb = np.stack([r, g, b], axis=2)
+    #     return rgb
 
 
 
