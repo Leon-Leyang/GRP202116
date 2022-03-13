6cf445d6e68503dc59f9d96c0bf7587727160236
Leon-Leyang <scylh6@nottingham.edu.cn>
2022-03-13 22:20:10 +0800

    Implement object detection model

--- flask-server/Models/ObjDecBBoxModel.py
+++ flask-server/Models/ObjDecBBoxModel.py
@@ -29,11 +29,8 @@ class ObjDecBBoxModel(Model):
 
         img = Image.open(imgPath)
 
-        # For development checking
-        orig = (np.array(img)).copy()
-        orig = cv2.cvtColor(orig, cv2.COLOR_RGB2BGR)
-
-        COLORS = np.random.uniform(0, 255, size=(len(self.labels), 3))
+        # Get the width and height of the image
+        imgWidth, imgHeight = img.size
 
         imgVec = self.transforms(img).unsqueeze(0).to(self.device)
         modelOutput = self.model(imgVec)[0]
@@ -42,39 +39,33 @@ class ObjDecBBoxModel(Model):
             index = int(modelOutput["labels"][i])
             confidence = modelOutput['scores'][i]
             if(confidence > self.threashold):
+                # Get the bounding box
                 box = modelOutput["boxes"][i].detach().cpu().numpy()
                 (startX, startY, endX, endY) = box.astype("int")
 
-                label = self.labels[index]
+                # Get the relative x, y, width and height
+                xPix = (startX / imgWidth) * 100
+                yPix = (startY / imgHeight) * 100
+                widthPix = (endX - startX) / imgWidth * 100
+                heightPix = (endY - startY) / imgHeight * 100
 
-                cv2.rectangle(orig, (startX, startY), (endX, endY),
-                              COLORS[index], 2)
-                y = startY - 15 if startY - 15 > 15 else startY + 15
-                cv2.putText(orig, label, (startX, y),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[index], 2)
+                value = {}
+                value['rectanglelabels'] = [self.labels[index]]
+                value['width'] = widthPix
+                value['height'] = heightPix
+                value['x'] = xPix
+                value['y'] = yPix
 
-        cv2.imshow("Output", orig)
-        cv2.waitKey(0)
+                resultItem = {}
+                resultItem['id'] = str(uuid.uuid4())
+                resultItem['from_name'] = self.fromName
+                resultItem['to_name'] = self.toName
+                resultItem['type'] = self.type
+                resultItem['value'] = value
 
+                self.predictionItem['result'].append(resultItem)
 
-        # Get the index of the most likely class
-        # index = torch.max(modelOutput, 1)[1][0]
-
-        # Get the percentage of the prediction
-        # percentage = (torch.nn.functional.softmax(modelOutput, dim=1)[0] * 100)[index].item()
-        #
-        # value = {}
-        # value['choices'] = [labels[index]]
-        # resultItem = {}
-        # resultItem['id'] = str(uuid.uuid4())
-        # resultItem['from_name'] = self.fromName
-        # resultItem['to_name'] = self.toName
-        # resultItem['type'] = self.type
-        # resultItem['value'] = value
-        #
-        # self.predictionItem['result'].append(resultItem)
-        #
-        # return self.predictionItem
+        return self.predictionItem
 
 
 if __name__ == '__main__':
@@ -93,4 +84,4 @@ if __name__ == '__main__':
     objDecBBoxModel = ObjDecBBoxModel(modelPath, modelVersion, fromName, toName, toolType, labels, threashold)
 
     predictionItem = objDecBBoxModel.predict(imgPath)
-    # print(predictionItem)
+    print(predictionItem)
