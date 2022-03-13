ea81384eddf6625b809fa1df34561708706ef810
scyyc7 <scyyc7@nottingham.edu.cn>
2022-03-13 16:30:46 +0800

    Model transfer updated.

--- consumer-server/src/main/java/com/grp202116/backend/controller/TestController.java
+++ consumer-server/src/main/java/com/grp202116/backend/controller/TestController.java
@@ -1,11 +1,8 @@
 package com.grp202116.backend.controller;
 
-import feign.Request;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.alibaba.fastjson.JSONObject;
+import com.grp202116.backend.util.HttpUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigInteger;
@@ -16,8 +13,11 @@ import java.math.BigInteger;
 @RestController
 public class TestController {
 
-    @Autowired
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
+
+    public TestController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     @RequestMapping("/java-user")
     public String JavaUser() {
@@ -32,6 +32,39 @@ public class TestController {
     @GetMapping("/model/{modelId}")
     public String ModelTest(@PathVariable BigInteger modelId) {
         restTemplate.getForEntity("http://sidecar-server/model/" + modelId, String.class).getBody();
+        System.out.println(restTemplate.getForEntity("http://sidecar-server/model/" + modelId, String.class).getBody());
         return "{Success}";
+    }
+
+    //@PathVariable BigInteger modelId
+    //project_type, data, configs, predictions, model_path, model_version, labels):
+    @PostMapping("/model")
+    public String runModel() {
+        String configs = "<View>" +
+                "  <Image name=\" image \" value=\" $image \" zoom=\" true \"/>" +
+                " <BrushLabels name=\" tag \" toName=\" image \">" +
+                "     <Label value=\" Airplane \" background=\" rgba(255, 0, 0, 0.7) \"/>" +
+                "                               <Label value=\" Car \" background=\" rgba(0, 0, 255, 0.7) \"/>" +
+                "                           </BrushLabels>" +
+                "                       </View>";
+
+        String labels = "'Background', 'Aeroplane', 'Bicycle', 'Bird', 'Boat', 'Bottle', 'Bus', 'Car', 'Cat', 'Chair', 'Cow'," +
+                "'Dining table', 'Dog', 'Horse', 'Motorbike', 'Person', 'Potted plant', 'Sheep', 'Sofa', 'Train'," +
+                " 'Tv/monitor'";
+
+
+        JSONObject param = new JSONObject();
+        param.put("project_type", "Semantic Segmentation Mask");
+        param.put("data", "./puppy.webp");
+        param.put("configs", configs);
+        param.put("predictions", "");
+        param.put("model_path", "../ml/models/fcn/fcn.pth");
+        param.put("model_version", "undefined");
+        param.put("labels", labels);
+
+        String response = restTemplate.postForObject("http://sidecar-server/model",
+                HttpUtils.parseJsonToFlask(JSONObject.toJSONString(param)), String.class);
+        System.out.println(JSONObject.parse(response));
+        return response;
     }
 }
