db060fd13d2eb74c1378f0e4a1b9bda41ccf505f
Leon-Leyang <scylh6@nottingham.edu.cn>
2022-03-21 18:02:49 +0800

    Implement keypoint labeling model

---
+++ flask-server/Models/KpLabModel.py
@@ -0,0 +1,74 @@
+from PIL import Image
+
+import uuid
+import torchvision.transforms as tf
+
+from Models.Model import Model
+
+# Model for keypoint labeling task
+class KpLabModel(Model):
+    def __init__(self, modelPath, modelVersion, modelRoot, fromName, toName, toolType, labelsPath, threashold):
+        super().__init__(modelPath, modelVersion, modelRoot, fromName, toName, toolType, labelsPath)
+
+        # Preprocessing operations
+        self.transforms = tf.Compose([
+            tf.ToTensor()])
+
+        # Threshold to filter result
+        self.threashold = threashold
+
+    def predict(self, imgPath):
+        super().predict()
+
+        img = Image.open(imgPath)
+
+        # Get the width and height of the image
+        imgWidth, imgHeight = img.size
+
+        imgVec = self.transforms(img).unsqueeze(0).to(self.device)
+        modelOutput = self.model(imgVec)
+
+        for item in modelOutput:
+            if(item['scores'] > self.threashold):
+                keypoints = item['keypoints'].cpu().detach().numpy().squeeze()
+                for idx, keypoint in enumerate(keypoints):
+                    label = self.labels[idx]
+                    x = keypoint[0]
+                    y = keypoint[1]
+                    xPix = (x / imgWidth) * 100
+                    yPix = (y / imgHeight) * 100
+                    value = {}
+                    value['keypointlabels'] = [label]
+                    value['x'] = xPix
+                    value['y'] = yPix
+                    # TODO: Set the width as defined in the frontend
+                    value['width'] = 0.3
+
+                    resultItem = {}
+                    resultItem['id'] = str(uuid.uuid4())
+                    resultItem['from_name'] = self.fromName
+                    resultItem['to_name'] = self.toName
+                    resultItem['type'] = self.type
+                    resultItem['value'] = value
+
+                    self.predictionItem['result'].append(resultItem)
+
+        return self.predictionItem
+
+
+if __name__ == '__main__':
+    modelPath = '../../ml/models/keypointrcnn/keypointrcnn.pth'
+    modelVersion = 'one'
+    modelRoot = 'C:/Users/Leon/Desktop/Keypoint-Labeling'
+    fromName = 'kp-1'
+    toName = 'img-1'
+    toolType = 'keypointlabels'
+    labelsPath = '../cocoKp.txt'
+    threashold = 0.9
+
+    imgPath = '../kid.jpg'
+
+    kpLabModel = KpLabModel(modelPath, modelVersion, modelRoot, fromName, toName, toolType, labelsPath, threashold)
+
+    predictionItem = kpLabModel.predict(imgPath)
+    print(predictionItem)
