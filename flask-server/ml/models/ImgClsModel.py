e16c4a017109032d07f4495f367c88437fbe852e
Leon-Leyang <scylh6@nottingham.edu.cn>
2022-03-27 19:26:12 +0800

    Test training of ImgClsModel

--- flask-server/ml/models/ImgClsModel.py
+++ flask-server/ml/models/ImgClsModel.py
@@ -1,11 +1,16 @@
 from PIL import Image
+from torch.utils.data import DataLoader, random_split
+from torch.optim import Adam
+from torch.autograd import Variable
 
 import uuid
 import torch
 import torchvision.transforms as tf
+import torch.nn as nn
 
 from ml import Preprocess
 from ml.models.Model import Model
+from ml.ImgClsDataset import ImgClsDataset
 
 # Model for image classification task
 class ImgClsModel(Model):
@@ -52,8 +57,102 @@ class ImgClsModel(Model):
 
         return self.predictionItem
 
-    # def train(self):
-    #     super().train()
+
+
+
+    def train(self, epochNum, splitParam, batchSize, shuffle, workerNum, learningRate):
+        # Function to test the model with the test dataset and print the accuracy for the test images
+        def testAccuracy():
+
+            self.model.eval()
+            accuracy = 0.0
+            total = 0.0
+
+            with torch.no_grad():
+                for data in self.testLoader:
+                    images, truths = data
+                    images = images.to(self.device)
+                    truths = truths.to(self.device)
+                    # print('truths: ' + self.labels[truths[0]])
+
+                    # run the model on the test set to predict labels
+                    outputs = self.model(images)
+                    # the label with the highest energy will be our prediction
+                    _, predicted = torch.max(outputs.data, 1)
+                    # print('predictions: ' + self.labels[predicted[0]] + '\n')
+                    total += truths.size(0)
+                    accuracy += (predicted == truths).sum().item()
+
+            # compute the accuracy over all test images
+            accuracy = (100 * accuracy / total)
+            return (accuracy)
+
+        def getAnnotations():
+            pass
+
+
+        super().train()
+
+        #TODO: Get annotations from database
+        # annotations = getAnnotations()
+        annotations = [
+            {'result': [{'id': '6c3606b2-bfce-48c2-b66d-6d58998efaf1', 'from_name': 'choice', 'to_name': 'image', 'type': 'choices', 'value': {'choices': ['golden retriever']}}]},
+            {'result': [{'id': '6c3606b2-bfce-48c2-b66d-6d58998efaf1', 'from_name': 'choice', 'to_name': 'image', 'type': 'choices', 'value': {'choices': ['golden retriever']}}]},
+            {'result': [{'id': '6c3606b2-bfce-48c2-b66d-6d58998efaf1', 'from_name': 'choice', 'to_name': 'image', 'type': 'choices', 'value': {'choices': ['golden retriever']}}]},
+            {'result': [{'id': '6c3606b2-bfce-48c2-b66d-6d58998efaf1', 'from_name': 'choice', 'to_name': 'image', 'type': 'choices', 'value': {'choices': ['golden retriever']}}]},
+            {'result': [{'id': '6c3606b2-bfce-48c2-b66d-6d58998efaf1', 'from_name': 'choice', 'to_name': 'image', 'type': 'choices', 'value': {'choices': ['golden retriever']}}]},
+            {'result': [{'id': '6c3606b2-bfce-48c2-b66d-6d58998efaf1', 'from_name': 'choice', 'to_name': 'image', 'type': 'choices', 'value': {'choices': ['Persian cat']}}]},
+            {'result': [{'id': '6c3606b2-bfce-48c2-b66d-6d58998efaf1', 'from_name': 'choice', 'to_name': 'image', 'type': 'choices', 'value': {'choices': ['Persian cat']}}]},
+            {'result': [{'id': '6c3606b2-bfce-48c2-b66d-6d58998efaf1', 'from_name': 'choice', 'to_name': 'image', 'type': 'choices', 'value': {'choices': ['Persian cat']}}]},
+            {'result': [{'id': '6c3606b2-bfce-48c2-b66d-6d58998efaf1', 'from_name': 'choice', 'to_name': 'image', 'type': 'choices', 'value': {'choices': ['Persian cat']}}]},
+            {'result': [{'id': '6c3606b2-bfce-48c2-b66d-6d58998efaf1', 'from_name': 'choice', 'to_name': 'image', 'type': 'choices', 'value': {'choices': ['Persian cat']}}]}
+        ]
+
+        imgClsSet = ImgClsDataset(annotations, self.preprocess, self.labels)
+        trainNum = round(len(imgClsSet) * splitParam)
+        testNum = len(imgClsSet) - trainNum
+        trainSet, testSet = random_split(imgClsSet, lengths=[trainNum, testNum])
+        self.trainLoader = DataLoader(trainSet, batch_size=batchSize, shuffle=shuffle, num_workers=workerNum)
+        self.testLoader = DataLoader(testSet, batch_size=batchSize, shuffle=shuffle, num_workers=workerNum)
+
+        loss_fn = nn.CrossEntropyLoss()
+        #TODO: Generalize optimizer
+        optimizer = Adam(self.model.parameters(), lr=learningRate, weight_decay=0.0001)
+
+        for epoch in range(epochNum):  # loop over the dataset multiple times
+            running_loss = 0.0
+            running_acc = 0.0
+
+            for i, (images, truths) in enumerate(self.trainLoader, 0):
+
+                # get the inputs
+                images = Variable(images.to(self.device))
+                truths = Variable(truths.to(self.device))
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+                # predict classes using images from the training set
+                outputs = self.model(images)
+                # compute the loss based on model output and real labels
+                loss = loss_fn(outputs, truths)
+                # backpropagate the loss
+                loss.backward()
+                # adjust parameters based on the calculated gradients
+                optimizer.step()
+
+                # Let's print statistics for every 1,000 images
+                # running_loss += loss.item()  # extract the loss value
+                # if i % 1 == 0:
+                #     # print every 1000
+                #     print('[%d, %5d] loss: %.3f' %
+                #           (epoch + 1, i + 1, running_loss / 1))
+                #     # zero the loss
+                #     running_loss = 0.0
+
+            # Compute and print the average accuracy fo this epoch when tested over all 10000 test images
+            accuracy = testAccuracy()
+            print('For epoch', epoch + 1, 'the test accuracy over the whole test set is %d %%' % (accuracy))
+
 
 
 
@@ -72,8 +171,11 @@ if __name__ == '__main__':
     imgSize = 224
 
     imgPath = '../../../ml/resources/puppy.webp'
+    # imgPath = './persian-cat-5.jpg'
 
     imgClsModel = ImgClsModel(modelPath, modelVersion, modelRoot, fromName, toName, toolType, labelsPath, mean, std, imgSize)
 
-    predictionItem = imgClsModel.predict(imgPath)
-    print(predictionItem)
+    # predictionItem = imgClsModel.predict(imgPath)
+    # print(predictionItem)
+
+    imgClsModel.train(10, 0.1, 1, False, 1, 0.001)
