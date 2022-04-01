962251eeea92a3d6f5d4b61a213dc98d11f21e3f
LinjingSUN <scyls1@nottingham.edu.cn>
2022-04-01 11:31:58 +0800

    temp

--- frontend/src/views/PerProject/ML.vue
+++ frontend/src/views/PerProject/ML.vue
@@ -1,12 +1,218 @@
 <template>
-    <div>
-        <el-upload
-            action=""
-            multiple
-            :limit="3"
-            :file-list="[{name: 'name', url: 'url'}]">
-            <el-button size="small" type="primary">点击上传</el-button>
-            <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
-        </el-upload>
-    </div>
+<div style="font-size: 1.25rem; font-weight: 500"> 
+    <v-container class="ml-container">
+  <el-form ref="form" :model="form" :label-position="right"><!--why-->
+
+   <el-form-item label="Model Version:">
+    <el-input v-model="form.name" clearable maxlength="" size="small" suffix-icon="el-icon-edit el-input__icon"></el-input>
+  </el-form-item>
+
+  <el-form-item label="Model Type:">
+    <!--<el-select clearable placeholder="please select the model type" size="small" @change="lyj">
+      <el-option label="Image Classification" ></el-option>
+      <el-option label="Object Detection" ></el-option>
+      <el-option label="Keypoint Labeling" ></el-option>
+      <el-option label="Semantic Segmentation with Masks" ></el-option>
+      <el-option label="Text Classification" ></el-option>
+      <el-option label="Name Entity Recognition" ></el-option>
+      <el-option label="Customization" ></el-option>
+          <el-option
+                  v-for="type in typeOptions"
+                  :key="type.value"
+                  :label="type.label"
+                  :value="type.value"
+                ></el-option>
+    </el-select>-->
+
+     <el-select v-model="type" clearable @change="c1">
+    <el-option  v-for="item in typeOptions" :key="item.value" :label="item.label" :value="item.value"></el-option>
+ </el-select>
+  </el-form-item>
+
+
+<!--If choosing others, then it will not display the following ??-->
+  <el-form-item label="Model Project Root Path:">
+    <el-input v-model="form.name" clearable maxlength="" size="small" suffix-icon="el-icon-edit el-input__icon"></el-input>
+  </el-form-item>
+
+  <el-form-item label="Model File:" style="margin-top:20px">
+     <v-file-input
+                label="File Input"
+                outlined
+                dense
+                ></v-file-input>
+  </el-form-item>
+  
+  <el-form-item label="Class File:" style="margin-top:20px">
+     <v-file-input
+                label="File Input"
+                outlined
+                dense
+                ></v-file-input>
+  </el-form-item>
+  <!--end-->
+ 
+
+  <el-form-item label="Model Description:" style="margin-bottom:20px!important">
+    <el-input type="textarea" v-model="form.desc"></el-input>
+  </el-form-item>
+  
+  <!--
+  <el-form-item label="Upload Customized Backend:" style="margin-top:10px">
+  <v-file-input
+    label="File Input"
+    outlined
+    dense
+  ></v-file-input>
+  </el-form-item>
+  -->
+
+  <v-divider></v-divider>
+
+  <el-form-item label="image size:" > <!--style="margin-bottom:0"-->
+    <el-input v-model="form.name" clearable maxlength="" size="mini" placeholder="(For image classification)"></el-input>
+  </el-form-item>
+ <v-divider></v-divider>
+
+  <el-form-item v-if="isObjDetect" label="threashold:">
+    <el-input v-model="form.name" clearable maxlength="" size="mini" placeholder="(For object detection)"></el-input>
+  </el-form-item>
+ <v-divider></v-divider>
+
+  <el-form-item label="mean:">
+    <el-input v-model="form.name" clearable maxlength="" size="mini" placeholder="(For semantic segmentation) [0.485, 0.456, 0.406] by default"></el-input>
+  </el-form-item>
+  <el-form-item label="standard deviation:">
+    <el-input v-model="form.name" clearable maxlength="" size="mini" placeholder="[0.229, 0.224, 0.225] by default"></el-input>
+  </el-form-item>
+   <v-divider></v-divider>
+
+  <el-form-item label="threashold:">
+    <el-input v-model="form.name" clearable maxlength="" size="mini" placeholder="(For keypoint labeling)"></el-input>
+  </el-form-item>
+   <v-divider></v-divider>
+
+  <el-form-item label="vocabulary file:" style="margin-top:20px">
+    <v-file-input
+                label="File Input"
+                outlined
+                dense
+                ></v-file-input>
+  </el-form-item>
+  <el-form-item label="token number:">
+    <el-input v-model="form.name" clearable maxlength="" size="mini" placeholder="(For text classification)"></el-input>
+  </el-form-item>
+   <el-form-item label="sequence length:">
+    <el-input v-model="form.name" clearable maxlength="" size="mini" placeholder="(For text classification)"></el-input>
+  </el-form-item>
+   <v-divider></v-divider>
+
+  <el-form-item label="sequence length:">
+    <el-input v-model="form.name" clearable maxlength="" size="mini" placeholder="(For named entity recognition)"></el-input>
+  </el-form-item>
+
+  <el-form-item style="margin-top:40px">
+    <el-button type="primary" @click="onSubmit">Save</el-button>
+    <el-button>Cancel</el-button>
+  </el-form-item>
+
+  <div>
+        <el-button type="primary" @click="$router.push('/ML-Test')">Test</el-button>
+        <el-button type="primary" @click="$router.push('/ML-Train')">Train</el-button>
+  </div>
+
+</el-form>
+</v-container>
+</div>
+
+
 </template>
+
+<style>
+
+  .ml-container{
+      width:50%;
+      margin-left:0;
+  }
+
+  .el-select .el-input {
+    width: 230px;
+  }
+  .input-with-select .el-input-group__prepend {
+    background-color: #fff;
+  }
+  .content{
+      margin-left:20px;
+      width:90%;
+      margin-top:6px;
+  }
+
+.v-text-field--outlined fieldset {
+    border-collapse: collapse;
+    border-style: solid;
+    border-color: #DCDFE6!important;
+    border-width: 1px;
+    bottom: 0;/* */
+    top: -5px;/* */
+    /*height: 32px;*/
+    left: 0;
+    right: 0;
+    pointer-events: none;
+    position: absolute;
+    transition-duration: 0.15s;
+    transition-property: color;
+    transition-timing-function: cubic-bezier(0.25, 0.8, 0.25, 1);
+}
+
+  .v-input {
+    height: 40px;
+}
+  
+  .el-form-item {
+    margin-bottom: 0px!important;
+    margin-left: 20px;
+    margin-right: 20px;
+
+  }
+    
+</style>
+
+<script>
+  export default {
+    data() {
+      return {
+        form: {
+          name: '',
+          region: '',
+          desc: '',
+          type: [],
+          resource: ''
+        },
+
+       type:0,
+
+ typeOptions:[
+     {label:'Image Classification',value:0},
+     {label:'Object Detection',value:1},
+     {label:'Keypoint Labeling',value:2},
+     {label:'Semantic Segmentation with Masks',value:3},
+     {label:'Text Classification',value:4},
+     {label:'Name Entity Recognition',value:5},
+     {label:'Customization',value:6},
+  ],
+      }
+    },
+    methods: {
+      onSubmit() {
+        console.log('submit!');
+      },
+      c1(selectValue) {
+      if (selectValue == 1) {
+        this.isObjDetect = true;
+      } else {
+        this.isObjDetect = false;
+      }
+    },
+    }
+  }
+</script>
---
+++ frontend/src/views/PerProject/ML/ML-Test-.vue
@@ -0,0 +1,176 @@
+<template>
+
+<v-container>
+  <v-row ><!--justify="center"-->
+
+    <v-card>    <v-data-iterator
+      :items="items" 
+    >
+      
+
+      <template v-slot:default="props">
+        <v-row dense>
+          <v-col
+            v-for="item in props.items"
+            :key="item.name"
+            cols="12"
+            md="4"
+          >
+            
+                <v-card-subtitle>
+                    {{ item.name }}
+                </v-card-subtitle>
+
+                <v-card-subtitle>{{item.description}}</v-card-subtitle>
+               
+                <v-progress-linear
+                color="light-blue"
+                height="7"
+                rounded
+                :value="item.process"
+                style="width:270px; margin-left: 10px"
+                ></v-progress-linear>  
+               
+               
+                <v-progress-linear
+                color="light-blue"
+                height="7"
+                rounded
+                :value="item.process"
+                style="width:270px; margin-left: 10px"
+                ></v-progress-linear> 
+                
+
+                <v-card-actions>
+                <div style="margin-left:16px">
+                  <v-row>
+                    <v-card-subtitle style="padding:0px; font-size:10px; height: 15px; color: orange">
+                      {{item.createTime}}
+                    </v-card-subtitle>
+                  </v-row>
+                  <v-row>
+                    <v-card-subtitle style="padding:0px; font-size:10px; height: 15px; color: orange">
+                       {{item.updateTime}}
+                    </v-card-subtitle>
+                  </v-row>
+                </div>
+
+                <v-spacer></v-spacer>
+               
+                <v-btn>Test</v-btn>
+                <v-btn>Train</v-btn>
+                
+                
+                </v-card-actions>
+            
+          </v-col>
+       
+        </v-row>
+      </template>
+
+    </v-data-iterator></v-card>
+
+
+    <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="600px"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-card>
+            <v-container>
+        <v-btn
+          color="primary"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          TEST
+        </v-btn>
+        </v-container>
+        </v-card>
+      </template>
+
+
+      <v-card>
+        <v-card-title>
+          <span class="text-h5">Test</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+             <!--
+              <v-col cols="12">
+                <v-text-field
+                  label="Model Version*"
+                  required
+                ></v-text-field>
+              </v-col>
+             -->   
+              <v-col cols="12">
+                <v-file-input
+                label="Upload the Customized Train Script Here"
+                outlined
+                dense
+                ></v-file-input>
+              </v-col>
+
+            </v-row>
+          </v-container>
+          <!--
+          <small>*indicates required field</small>-->
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <!--
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="dialog = false"
+          >
+            Close
+          </v-btn>
+          -->
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="dialog = false"
+          >
+            Run
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    
+
+  </v-row>
+  </v-container>
+</template>
+
+<script>
+  import Mock from 'mockjs'
+  export default {
+    data () {
+      return {
+       
+        
+        keys: [
+          'Name',
+          'Process',
+        ],
+        items: [
+          {
+            name: "xxx",
+            description: "xxx",
+            process: Mock.Random.integer(0, 100),
+           
+          },
+        ],
+        dialog: false,
+      }
+    },
+    
+  }
+ 
+</script>
