6c853339f6eae631a692e462cd210cc03178e736
LinjingSUN <scyls1@nottingham.edu.cn>
2022-04-06 00:38:03 +0800

    export file
    but always export csv

--- frontend/src/views/PerProject.vue
+++ frontend/src/views/PerProject.vue
@@ -1,12 +1,23 @@
 <!-- 项目进入后的具体页面 -->
 <template>
   <v-card id="problock">
-<button @click="back">Back</button>
-<v-btn @click="deleteSelectBtn()"></v-btn>
+  <button @click="back">Back</button>
+  <button @click="download(id,name)" type="button"> Down Load </button>
+<!-- <v-btn @click="deleteSelectBtn()"></v-btn> -->
+    <v-radio-group v-model="format" row>
+      <v-radio
+        v-for="n in fileFormat"
+        :key="n"
+        :label="n"
+        :value="n"
+      ></v-radio>
+    </v-radio-group>
     <v-tabs
         v-model="tabs"
         centered
         grow
+        color="yellow"
+        background-color="rgb(113, 223, 207)"
     >
         <v-tab>
             Data
@@ -24,26 +35,27 @@
     <v-divider></v-divider>
     <v-tabs-items v-model="tabs">
       <v-tab-item>
-        <v-card flat v-if="tabs==0">
+        <v-card flat class="tabcard" v-if="tabs==0">
           <Table :dataList="dataList" :config="config" />
         </v-card>
       </v-tab-item>
       <v-tab-item>
-        <v-card flat v-if="tabs==1">
+        <v-card flat v-if="tabs==1" class="tabcard">
             <Statistics></Statistics>
         </v-card>
       </v-tab-item>
       <v-tab-item>
-        <v-card flat v-if="tabs==2">
+        <v-card flat v-if="tabs==2" class="tabcard">
           <ML></ML>
         </v-card>
       </v-tab-item>
       <v-tab-item>
-        <v-card flat v-if="tabs==3">
+        <v-card flat v-if="tabs==3" class="tabcard">
           <Setting></Setting>
         </v-card>
       </v-tab-item>
     </v-tabs-items>
+
   </v-card>
 </template>
 
@@ -63,6 +75,10 @@ import Setting from '@/views/PerProject/Setting'
     },
     data () {
       return {
+        format:'json',
+        fileFormat:['json','csv','tsv'],
+        id:null,
+        name:null,
         projectId:0,
         tabs: null,
         dataList:[],
@@ -88,7 +104,34 @@ import Setting from '@/views/PerProject/Setting'
           })
         }
 
+      },
+      download(id, name) {
+        console.log('format', this.format)
+        console.log('id$name', id, name)
+        this.$axios.get('/project/'+ id +'/data_export/annotations/csv', {
+          responseType: 'blob'
+        }).then((res)=> {
+          console.log('res', res)
+          const content = res.data;
+          const blob = new Blob([content])
+          console.log('content blob', content, blob)
+          if ('download' in document.createElement('a')) {
+            const elink = document.createElement('a');
+            elink.download = name;
+            elink.style.display = 'none';
+            elink.href = URL.createObjectURL(blob);
+            document.body.appendChild(elink);
+            elink.click();
+            URL.revokeObjectURL(elink.href);
+            document.body.removeChild(elink);
+          } else {
+            navigator.msSaveBlob(blob, name)
+          }
+        }, function(err) {
+          console.log('err', err)
+        })
       }
+
     },
     watch:{
       tabs:function(val){
@@ -112,6 +155,9 @@ import Setting from '@/views/PerProject/Setting'
       }
     },
     mounted() {
+      this.id = this.$store.state.currentProjectId
+      this.name = this.$store.state.currentProject.name
+      console.log('id name', this.id, this.name)
       this.$store.state.currentMLList = null
       console.log('label', this.$store.state.currentConfig)
       this.projectId = this.$store.state.currentProjectId
@@ -137,5 +183,18 @@ import Setting from '@/views/PerProject/Setting'
 #problock{
     margin: 0 auto;
     width: 90%;
+   /* height: 90%;*/
 }
+
+.tabcard{
+    height:480px/*100%*/;
+ 
+}
+
+.v-tabs-slider{
+  color:yellow;
+}
+
+
+
 </style>
