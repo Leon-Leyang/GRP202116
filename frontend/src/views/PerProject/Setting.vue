acc1acd08741a2206fb47f5cdae24971ef409262
LinjingSUN <scyls1@nottingham.edu.cn>
2022-04-05 09:23:49 +0800

    dialog

--- frontend/src/views/PerProject/Setting.vue
+++ frontend/src/views/PerProject/Setting.vue
@@ -79,18 +79,58 @@
       </v-icon>
     </v-btn>
 
-    <v-btn
-      class="mx-2"
-      fab
-      dark
-      small
-      color="cyan"
-      @click="editTemplate"
+
+    <v-dialog
+      v-model="dialog"
+      width="500"
     >
-      <v-icon dark>
-        mdi-pencil
-      </v-icon>
-    </v-btn>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          class="mx-2"
+          fab
+          dark
+          small
+          v-bind="attrs"
+          v-on="on"
+          color="cyan"
+          @click="editTemplate"
+        >
+          <v-icon dark>
+            mdi-pencil
+          </v-icon>
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title class="text-h5 grey lighten-2">
+          Privacy Policy
+        </v-card-title>
+
+        <v-card-text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            text
+            @click="dialog = false"
+          >
+            Cancel
+          </v-btn>          
+          <v-btn
+            color="primary"
+            text
+            @click="dialog = false"
+          >
+            Confirm
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 
     </v-col>
     </v-row>
@@ -160,6 +200,7 @@
 export default {
   data() {
     return {
+      dialog: false,
       items: [
                 {
                 title: 'Semantic Segmentation'+"\n"+' with Polygons',
@@ -286,7 +327,7 @@ export default {
         this.nowTemplateImage = this.items[i].img
       }
     }
-    console.log('ti,im', this.$store.state.currentConfigTitle,this.nowTemplateImage, this.$store.state.currentConfigTitle)
+    console.log('ti,im', this.$store.state.currentConfigTitle,this.nowTemplateImage, this.$store.state.currentConfig)
 },
 
 
