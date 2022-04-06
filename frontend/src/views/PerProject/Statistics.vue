a08abd6237dd4a8cd8c94a48e762149748a3b147
YJL <scyyl17@nottingham.edu.cn>
2022-04-06 11:27:08 +0800

    push the statisitics file with style in frontend

--- frontend/src/views/PerProject/Statistics.vue
+++ frontend/src/views/PerProject/Statistics.vue
@@ -1,49 +1,59 @@
 <template>
   <v-container class="co">
+ 
   <v-row
         no-gutters
         justify="center"
+        style="height:100%"
   >
-  <v-spacer></v-spacer> 
-  <v-col md="3">
+  
+  <v-col md="3"><!-- style="height:100%"-->
         
         <v-card
            class="c-1"
            outlined
+           tile
           >
           Data 
-        <el-row>
+        <el-row style="height:27%">
         <el-col :span="24" class="inblock">
-            <div class="grid-content bg-purple title">Data completed</div>
-            <div class="pagetext-a">{{statistic.labeledDataListNumber}}/{{statistic.dataListNumber}}</div>
+            <div class="grid-content bg-purple-light title">Total Data:</div>
+            <div class="pagetext-a">{{statistic.dataListNumber}}</div>
         </el-col>
-        </el-row>
+        </el-row> 
 
         <el-row>
         <el-col :span="24" class="inblock">
-            <div class="grid-content bg-purple title">Percentage completed</div>
+            <div class="grid-content bg-purple title">Percentage completed:</div>
             <div class="pagetext-b"><el-progress type="circle" :percentage="statistic.completionPercentage"></el-progress></div>
         </el-col>
         </el-row>
-
+       <!--
         <el-row>
         <el-col :span="24" class="inblock" v-if="this.$store.state.currentProject.type == 'text'">
             <div class="grid-content bg-purple-light title">Average word counts per piece of data </div>
             <div class="pagetext-a">{{statistic.averageTextWordsNumber}}</div>
         </el-col>
         </el-row>      
-
+       --> 
+       <el-row>
+        <el-col :span="24" class="inblock">
+            <div class="grid-content bg-purple title">Data completed:</div>
+            <div class="pagetext-a">{{statistic.labeledDataListNumber}}</div>
+        </el-col>
+        </el-row>
       </v-card>
           
   </v-col>
-  <v-spacer></v-spacer> 
+
   <v-col md="3">
            <v-card
             class="c-1"
             outlined
+            tile
           >
             Annotation
-        <el-row>
+        <el-row style="height:27%">
         <el-col :span="24" class="inblock">
             <div class="grid-content bg-purple title">Total annotations:</div>
             <div class="pagetext-a">{{statistic.annotationsNumber}}</div>
@@ -52,18 +62,18 @@
         <el-row>
         <el-col :span="24" class="inblock">
             <div class="grid-content bg-purple title">Annotations Percentage:</div>
-            <div class="pagetext-b"><el-progress type="circle" :percentage="statistic.averageAnnotations"></el-progress></div>
+            <div class="pagetext-b"><el-progress type="circle" :percentage="statistic.averageAnnotations" color="green"></el-progress></div>
         </el-col>
         </el-row>   
         <el-row>
         <el-col :span="24" class="inblock">
-            <div class="grid-content bg-purple-light title">Average annotations per data</div>
+            <div class="grid-content bg-purple-light title">Average annotations per piece of data:</div>
             <div class="pagetext-a">{{statistic.annotationsNumber/statistic.dataListNumber}}</div>
         </el-col>
         </el-row>
           </v-card>
   </v-col>
-  <v-spacer></v-spacer> 
+  
   <v-col md="3">
            <v-card
             class="c-1"
@@ -71,7 +81,7 @@
             tile
           >
             Prediction
-        <el-row>
+        <el-row style="height:27%">
         <el-col :span="24" class="inblock">
             <div class="grid-content bg-purple title">Total predictions:</div>
             <div class="pagetext-a">{{statistic.predictionsNumber}}</div>
@@ -81,14 +91,40 @@
         <el-row>
         <el-col :span="24" class="inblock">
             <div class="grid-content bg-purple title">Predictions Percentage:</div>
-            <div class="pagetext-b"><el-progress type="circle" :percentage="statistic.averagePredictions"></el-progress></div>
+            <div class="pagetext-b"><el-progress type="circle" :percentage="statistic.averagePredictions" color="red"></el-progress></div>
         </el-col>
         </el-row> 
 
         <el-row>  
         <el-col :span="24" class="inblock">
-            <div class="grid-content bg-purple-light title">Average predictions per data</div>
+            <div class="grid-content bg-purple-light title">Average predictions per piece of data:</div>
             <div class="pagetext-a">{{statistic.predictionsNumber/statistic.dataListNumber}}</div>
+        </el-col>
+        </el-row>
+   
+        </v-card>
+  </v-col>
+
+  <v-col md="3">
+           <v-card
+            class="c-1"
+            outlined
+            tile
+          >
+            ML Model
+        <el-row style="height:45%">
+        <el-col :span="24" class="inblock">
+            <div class="grid-content bg-purple title">Part1</div>
+            <div class=""> </div>
+        </el-col>
+        </el-row>
+
+         
+
+        <el-row>  
+        <el-col :span="24" class="inblock">
+            <div class="grid-content bg-purple-light title">Part2</div>
+            <div class=""></div>
         </el-col>
         </el-row>
    
@@ -124,46 +160,53 @@ export default {
 
 <style>
 
-
 .co{
-    background:rgb(228, 92, 92);
     padding: 0;
     margin: 0;
     max-width: 1262.9px;
+    height: 100%;
 }
 .c-1{
     height: 100%;
-    border-color: rebeccapurple!important;;
+    border-color: /*rgb(35, 157, 232)*/rgb(113, 223, 207)!important;
     border-width: 3px!important;
+    background:rgb(218, 253, 249)!important;
     /*width:70%;*/
 }
 
 .inblock{
-    height: /*230px*/160px;/*180px*/
+    height: /*230px*/33%;/*180px*/
 }
+
+.inblocka{
+    height: /*230px*/33%;/*180px*/
+}
+
 .title{
     font-style:italic;
     font-size:0.8em;
-    text-decoration :underline;
+    font-family:Georgia, serif!important;
+    /*text-decoration :underline;*/
     color: rgb(124,124,124);
 }
 .pagetext-a{
-    height: 50px;
+   /* height: 50px; */
     font-size:2.5em;
     font-weight: 600;
     text-align: center;
-    margin-top: 30px;/*45px*/
-    margin-bottom: 45px;/* */  
+    height: 33%;
+    /*margin-top: 30px;45px*/
+    /*margin-bottom: 45px; */  
 }
 
 .pagetext-b{
-    height: 50px;
+    /*height: 50px;*/
+    height: 33%;
     font-size:2.5em;
     font-weight: 600;
     text-align: center;
-    margin-top: 15px;/*45px*/
-    margin-bottom: 45px;/* */
+    margin-top: 5px;/*45px*/
+    /*margin-bottom: 45px; */
     
 }
-
 </style>
